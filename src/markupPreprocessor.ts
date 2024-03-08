a8a453052558d72958e919a55b09ca4be632ee29
babakfp <babak.bxf@gmail.com>
2024-03-08 14:09:03 -0800

    Added `isFileIgnored()`

--- src/markupPreprocessor.ts
+++ src/markupPreprocessor.ts
@@ -2,13 +2,30 @@ import type { MarkupPreprocessor } from "svelte/compiler"
 
 import type { Config } from "./types.js"
 
+const isFileIgnored = (
+    filename: Parameters<MarkupPreprocessor>[0]["filename"],
+    extensions: Config["extensions"]
+) => {
+    if (!filename) {
+        return true
+    }
+
+    if (filename.includes("/.svelte-kit/")) {
+        return true
+    }
+
+    for (const extension of extensions) {
+        if (!filename.endsWith(extension)) {
+            return true
+        }
+    }
+
+    return false
+}
+
 export const markupPreprocessor = (extensions: Config["extensions"]) => {
     return ((options) => {
-        if (!options.filename) return
-        if (options.filename.includes("/.svelte-kit/")) return
-        for (const extension of extensions) {
-            if (!options.filename.endsWith(extension)) return
-        }
+        if (isFileIgnored(options.filename, extensions)) return
         // ...
     }) satisfies MarkupPreprocessor
 }
