50d30c0bae91cca0ea37c1de83c47c0134ab8563
babakfp <babak.bxf@gmail.com>
2024-03-24 16:45:17 +0330

    Added `getTableOfContents()`

---
+++ src/getTableOfContents.ts
@@ -0,0 +1,53 @@
+export type TableOfContents = {
+    level: string
+    textContent: string
+    attributes: { [k: string]: string | null }
+}[]
+
+export type Options = (
+    | {
+          containerElement?: Element
+          containerSelector?: never // WTF TypeScript!
+      }
+    | {
+          containerElement?: never // WTF TypeScript!
+          containerSelector?: `#${string}` | `.${string}`
+      }
+) & { headingLevels?: (1 | 2 | 3 | 4 | 5 | 6)[] }
+
+export const getTableOfContents = (options?: Options) => {
+    const headingsDataItems: TableOfContents = []
+
+    const containerElement = options?.containerElement || document.body
+    const headingLevels = options?.headingLevels || [1, 2, 3, 4, 5, 6]
+
+    if (!headingLevels.length) return []
+
+    const selectors = headingLevels
+        .map((level) => {
+            if (!options?.containerSelector) return `h${level}`
+            return `${options?.containerSelector} h${level}`
+        })
+        .join(", ")
+
+    const headingElements = containerElement.querySelectorAll(selectors)
+
+    const filterHeadingAttributes = (headingElement: Element) => {
+        return Object.fromEntries(
+            Object.entries(headingElement.attributes).map(([_, v]) => [
+                v.nodeName,
+                v.nodeValue,
+            ])
+        )
+    }
+
+    headingElements.forEach((headingElement) => {
+        headingsDataItems.push({
+            level: headingElement.tagName[1],
+            textContent: headingElement.textContent ?? "",
+            attributes: filterHeadingAttributes(headingElement),
+        })
+    })
+
+    return headingsDataItems
+}
