a73e4204fee0ada8c38e31a2c397ae2aa17345ef
Babak Farkhoopak <44144724+babakfp@users.noreply.github.com>
2024-04-10 23:30:11 +0330

    Enhanced "remark-text-to-html" plugin

--- src/transformers/unified/plugins/remark-text-to-html.ts
+++ src/transformers/unified/plugins/remark-text-to-html.ts
@@ -22,12 +22,20 @@ To this:
 ```
 
 So that "rehype-stringify" doesn't escape it!
+If the value contains HTML tags, then it wil be converted to type of "html".
 */
 export default (): Transformer<Root> => {
     return (tree) => {
         visit(tree, "text", (node, index, parent) => {
             if (!parent || index === undefined) return
-            if (!node.value.startsWith("</")) return
+
+            if (
+                !node.value.startsWith("</") &&
+                !node.value.match(
+                    /<\/?[a-z][a-z0-9]*(?:\:[a-z][a-z0-9]*)*.*?\s*\/?>/
+                )
+            )
+                return
 
             parent.children.splice(index, 1, {
                 type: "html",
