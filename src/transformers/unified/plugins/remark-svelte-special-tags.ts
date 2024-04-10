6087216f369c691d6adb0489e8ac5f08f09dd894
Babak Farkhoopak <44144724+babakfp@users.noreply.github.com>
2024-04-10 23:44:52 +0330

    Improved comment

--- src/transformers/unified/plugins/remark-svelte-special-tags.ts
+++ src/transformers/unified/plugins/remark-svelte-special-tags.ts
@@ -2,7 +2,10 @@ import { SKIP, visit } from "unist-util-visit"
 import type { Transformer } from "unified"
 import type { Root } from "mdast"
 
-// This fixes the issue of Svelte Special Tags like `<svelte:head>` being parsed as markdown links.
+/*
+This fixes the issue of Svelte Special Tags like `<svelte:head>` being parsed as markdown links.
+https://github.com/orgs/remarkjs/discussions/1308#discussioncomment-9075147
+*/
 export default (): Transformer<Root> => {
     return (tree) => {
         visit(tree, "link", (node, index, parent) => {
