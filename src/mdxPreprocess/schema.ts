8f8185859e70e3026cbd82a8a34a24c8fa3b5d32
babakfp <babak.bxf@gmail.com>
2024-07-01 22:35:22 +0330

    Updated scheme custom error messages

--- src/mdxPreprocess/schema.ts
+++ src/mdxPreprocess/schema.ts
@@ -8,55 +8,26 @@ export const mdxPreprocessSchema = z
          * [".md", ".svelte.md"]
          */
         extensions: z
-            .string({
-                invalid_type_error:
-                    "The property of `extensions` in the first argument of the `mdxPreprocess(options)` function, must be a string. Valid extensions are `.md` and `.svelte.md`.",
-            })
+            .string()
             .regex(
                 /^\.[a-z]+(\.[a-z]+)?$/,
-                "The property of `extensions` in the first argument of the `mdxPreprocess(options)` function, received an invalid extension. Each extension must start with a dot and contain only lowercase letters. Valid extensions are `.md` and `.svelte.md`.",
+                'Invalid. Examples: ".md", ".svelte.md"',
             )
             .array()
-            .min(
-                1,
-                "The property of `extensions` in the first argument of the `mdxPreprocess(options)` function, must contain at least one extension.",
-            )
+            .min(1)
             .default([DOT_MD]),
         layouts: z
             .record(
+                z.string().min(1),
                 z
-                    .string({
-                        invalid_type_error:
-                            "A layout name, in the property of `layouts` in the first argument of the `mdxPreprocess(options)` function, must be a string.",
-                    })
-                    .min(
-                        1,
-                        "A layout name, in the property of `layouts` in the first argument of the `mdxPreprocess(options)` function, must be a non-empty string.",
-                    ),
-                z
-                    .string({
-                        invalid_type_error:
-                            "A custom element name, in the property of `layouts` in the first argument of the `mdxPreprocess(options)` function, must be a string.",
-                    })
-                    .regex(
-                        /[a-z]/,
-                        "A custom element name, in the property of `layouts` in the first argument of the `mdxPreprocess(options)` function, can't me empty and can only contain lowercase letters.",
-                    )
+                    .string()
+                    .min(1)
+                    .regex(/[a-z]/, "Only lowercase letters")
                     .array()
                     .default([]),
             )
             .optional(),
-        preprocessDependencies: z
-            .string({
-                invalid_type_error:
-                    "A dependency name, in the property of `preprocessDependencies` in the first argument of the `mdxPreprocess(options)` function, must be a string.",
-            })
-            .min(
-                1,
-                "A dependency name, in the property of `preprocessDependencies` in the first argument of the `mdxPreprocess(options)` function, must be a non-empty string.",
-            )
-            .array()
-            .default([]),
+        preprocessDependencies: z.string().min(1).array().default([]),
     })
     .passthrough()
     .default({})
