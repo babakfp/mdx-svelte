f3fd96d312217a40955a3b4c52f8425bfc9ff194
babakfp <babak.bxf@gmail.com>
2024-07-01 20:24:25 +0330

    Renamed EXTENSIONS

--- src/helpers/constants.ts
+++ src/helpers/constants.ts
@@ -1,3 +1,3 @@
 export const DOT_SVELTE = ".svelte"
 export const DOT_MD = ".md"
-export const SVELTE_CONFIG_EXTENSIONS = [DOT_SVELTE, DOT_MD] as const
+export const EXTENSIONS = [DOT_SVELTE, DOT_MD] as const
