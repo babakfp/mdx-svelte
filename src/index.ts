66798f0f400b2124af824ffa8d17b1f1e0561f97
babakfp <babak.bxf@gmail.com>
2024-07-01 23:43:01 +0330

    moved types file

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
 export * from "./helpers/constants.js"
 export * from "./helpers/makeToc.js"
 export * from "./mdxPreprocess/index.js"
-export type * from "./types/index.js"
+export type * from "./types.js"
