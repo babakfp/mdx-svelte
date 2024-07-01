f5e5ad59c506ff5786b61d99c3ebb8b0901b11fc
babakfp <babak.bxf@gmail.com>
2024-07-01 23:23:55 +0330

    .

--- play/3.js
+++ play/3.js
@@ -1,15 +1,13 @@
 import { mdxPreprocess } from "../dist/index.js"
 
-const FILE_NAME = "./play/1.svelte.md"
 const FILE_CONTENT = `
 # Hello, World!
 `
 
 const mdxMarkupPreprocess = mdxPreprocess()
 
-const { code } = await mdxMarkupPreprocess.markup({
-    filename: FILE_NAME,
+const result = await mdxMarkupPreprocess.markup({
     content: FILE_CONTENT,
 })
 
-console.log(code)
+console.log(result.code)
