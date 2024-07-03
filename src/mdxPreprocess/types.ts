13e30a863bb91426eb85698af6676b2a0de3de5a
babakfp <babak.bxf@gmail.com>
2024-07-03 18:34:28 +0330

    Removed export

--- src/mdxPreprocess/types.ts
+++ src/mdxPreprocess/types.ts
@@ -4,7 +4,7 @@ import type { z } from "zod"
 import type { mdxPreprocessSchema } from "../mdxPreprocess/schema.js"
 
 /** MDX Svelte callback options. */
-export type MdxPreprocessCallbacks = {
+type MdxPreprocessCallbacks = {
     /**
      * Callback function to determine whether a file should be ignored during preprocessing.
      * It runs after the `preprocessDependencies` option.
