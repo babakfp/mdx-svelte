86592a134f48243f3748e627928eeed5286b3521
babakfp <babak.bxf@gmail.com>
2024-07-31 08:59:18 +0330

    Improved remark-github-alerts

--- src/transformers/unified/plugins/remark-github-alerts/src/index.ts
+++ src/transformers/unified/plugins/remark-github-alerts/src/index.ts
@@ -1,115 +1,131 @@
 /*
- * THIS PLUGIN IS A FORK OF https://github.com/hyoban/remark-github-alerts PLUGIN.
- * @contributors https://github.com/hyoban/remark-github-alerts/graphs/contributors and https://github.com/babakfp.
+ * This plugin is a fork of the original [remark-github-alerts](https://github.com/hyoban/remark-github-alerts) plugin.
+ * Contributors can be found at [remark-github-alerts contributors](https://github.com/hyoban/remark-github-alerts/graphs/contributors) and [babakfp](https://github.com/babakfp).
  */
 
-import type { Paragraph, Root } from "mdast"
+import type { Root } from "mdast"
 import type { Transformer } from "unified"
 import { visit } from "unist-util-visit"
+import { capitalize } from "./capitalize.js"
 
 export type RemarkGitHubAlertsOptions = {
     /**
-     * List of markers to match.
+     * List of markers to match. Use `"*"` to match any markers.
      * @default
-     * ["TIP", "NOTE", "IMPORTANT", "WARNING", "CAUTION"]
+     * ["NOTE", "TIP", "IMPORTANT", "WARNING", "CAUTION"]
      */
-    markers?: string[] | "*"
+    markers?: ((typeof DEFAULT_MARKERS)[number] | string)[] | "*"
 
     /**
-     * If markers case sensitively on matching.
+     * Enable case-sensitive matching for markers.
      * @default false
      */
     matchCaseSensitive?: boolean
 
     /**
-     * Custom icons for each marker. The key is the marker name, and the value is the html script represent the icon.
-     * The key is always lowercase.
-     * @default inline svg icons from GitHub
+     * Custom icons for each marker.
+     * The key is the marker name (always lowercase), and the value is the HTML string representing the icon.
+     * Default icons are inline SVGs from GitHub.
      */
-    icons?: Record<string, string>
+    icons?: Record<(typeof DEFAULT_MARKERS)[number] | string, string>
 
     /**
-     * Custom titles for each marker. The key is the marker name, and the value is the title.
-     * The key is always lowercase.
-     * When the title is not specified, the default title is the capitalized marker name.
+     * Custom titles for each marker.
+     * The key is the marker name (always lowercase), and the value is the custom title.
+     * If not specified, the default title is the capitalized marker name.
      */
-    titles?: Record<string, string>
-
-    // ^ options from MarkdownItGitHubAlertsOptions
+    titles?: Record<(typeof DEFAULT_MARKERS)[number] | string, string>
 
     /**
-     * Whether to ignore the square brackets in the marker.
+     * Ignore square brackets in the marker.
      * @default false
      */
     ignoreSquareBracket?: boolean
 }
 
-function capitalize(str: string) {
-    return str.charAt(0).toUpperCase() + str.slice(1)
-}
+const DEFAULT_MARKERS = [
+    "NOTE",
+    "TIP",
+    "IMPORTANT",
+    "WARNING",
+    "CAUTION",
+] as const
 
-const DEFAULT_GITHUB_ICONS = {
-    note: '<svg xmlns="http://www.w3.org/2000/svg" aria-hidden="true" viewBox="0 0 16 16"><path d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8Zm8-6.5a6.5 6.5 0 1 0 0 13 6.5 6.5 0 0 0 0-13ZM6.5 7.75A.75.75 0 0 1 7.25 7h1a.75.75 0 0 1 .75.75v2.75h.25a.75.75 0 0 1 0 1.5h-2a.75.75 0 0 1 0-1.5h.25v-2h-.25a.75.75 0 0 1-.75-.75ZM8 6a1 1 0 1 1 0-2 1 1 0 0 1 0 2Z"/></svg>',
-    tip: '<svg xmlns="http://www.w3.org/2000/svg" aria-hidden="true" viewBox="0 0 16 16"><path d="M8 1.5c-2.363 0-4 1.69-4 3.75 0 .984.424 1.625.984 2.304l.214.253c.223.264.47.556.673.848.284.411.537.896.621 1.49a.75.75 0 0 1-1.484.211c-.04-.282-.163-.547-.37-.847a8.456 8.456 0 0 0-.542-.68c-.084-.1-.173-.205-.268-.32C3.201 7.75 2.5 6.766 2.5 5.25 2.5 2.31 4.863 0 8 0s5.5 2.31 5.5 5.25c0 1.516-.701 2.5-1.328 3.259-.095.115-.184.22-.268.319-.207.245-.383.453-.541.681-.208.3-.33.565-.37.847a.751.751 0 0 1-1.485-.212c.084-.593.337-1.078.621-1.489.203-.292.45-.584.673-.848.075-.088.147-.173.213-.253.561-.679.985-1.32.985-2.304 0-2.06-1.637-3.75-4-3.75ZM5.75 12h4.5a.75.75 0 0 1 0 1.5h-4.5a.75.75 0 0 1 0-1.5ZM6 15.25a.75.75 0 0 1 .75-.75h2.5a.75.75 0 0 1 0 1.5h-2.5a.75.75 0 0 1-.75-.75Z"/></svg>',
-    important:
+const DEFAULT_ICONS = {
+    NOTE: '<svg xmlns="http://www.w3.org/2000/svg" aria-hidden="true" viewBox="0 0 16 16"><path d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8Zm8-6.5a6.5 6.5 0 1 0 0 13 6.5 6.5 0 0 0 0-13ZM6.5 7.75A.75.75 0 0 1 7.25 7h1a.75.75 0 0 1 .75.75v2.75h.25a.75.75 0 0 1 0 1.5h-2a.75.75 0 0 1 0-1.5h.25v-2h-.25a.75.75 0 0 1-.75-.75ZM8 6a1 1 0 1 1 0-2 1 1 0 0 1 0 2Z"/></svg>',
+    TIP: '<svg xmlns="http://www.w3.org/2000/svg" aria-hidden="true" viewBox="0 0 16 16"><path d="M8 1.5c-2.363 0-4 1.69-4 3.75 0 .984.424 1.625.984 2.304l.214.253c.223.264.47.556.673.848.284.411.537.896.621 1.49a.75.75 0 0 1-1.484.211c-.04-.282-.163-.547-.37-.847a8.456 8.456 0 0 0-.542-.68c-.084-.1-.173-.205-.268-.32C3.201 7.75 2.5 6.766 2.5 5.25 2.5 2.31 4.863 0 8 0s5.5 2.31 5.5 5.25c0 1.516-.701 2.5-1.328 3.259-.095.115-.184.22-.268.319-.207.245-.383.453-.541.681-.208.3-.33.565-.37.847a.751.751 0 0 1-1.485-.212c.084-.593.337-1.078.621-1.489.203-.292.45-.584.673-.848.075-.088.147-.173.213-.253.561-.679.985-1.32.985-2.304 0-2.06-1.637-3.75-4-3.75ZM5.75 12h4.5a.75.75 0 0 1 0 1.5h-4.5a.75.75 0 0 1 0-1.5ZM6 15.25a.75.75 0 0 1 .75-.75h2.5a.75.75 0 0 1 0 1.5h-2.5a.75.75 0 0 1-.75-.75Z"/></svg>',
+    IMPORTANT:
         '<svg xmlns="http://www.w3.org/2000/svg" aria-hidden="true" viewBox="0 0 16 16"><path d="M0 1.75C0 .784.784 0 1.75 0h12.5C15.216 0 16 .784 16 1.75v9.5A1.75 1.75 0 0 1 14.25 13H8.06l-2.573 2.573A1.458 1.458 0 0 1 3 14.543V13H1.75A1.75 1.75 0 0 1 0 11.25Zm1.75-.25a.25.25 0 0 0-.25.25v9.5c0 .138.112.25.25.25h2a.75.75 0 0 1 .75.75v2.19l2.72-2.72a.749.749 0 0 1 .53-.22h6.5a.25.25 0 0 0 .25-.25v-9.5a.25.25 0 0 0-.25-.25Zm7 2.25v2.5a.75.75 0 0 1-1.5 0v-2.5a.75.75 0 0 1 1.5 0ZM9 9a1 1 0 1 1-2 0 1 1 0 0 1 2 0Z"/></svg>',
-    warning:
+    WARNING:
         '<svg xmlns="http://www.w3.org/2000/svg" aria-hidden="true" viewBox="0 0 16 16"><path d="M6.457 1.047c.659-1.234 2.427-1.234 3.086 0l6.082 11.378A1.75 1.75 0 0 1 14.082 15H1.918a1.75 1.75 0 0 1-1.543-2.575Zm1.763.707a.25.25 0 0 0-.44 0L1.698 13.132a.25.25 0 0 0 .22.368h12.164a.25.25 0 0 0 .22-.368Zm.53 3.996v2.5a.75.75 0 0 1-1.5 0v-2.5a.75.75 0 0 1 1.5 0ZM9 11a1 1 0 1 1-2 0 1 1 0 0 1 2 0Z"/></svg>',
-    caution:
+    CAUTION:
         '<svg xmlns="http://www.w3.org/2000/svg" aria-hidden="true" viewBox="0 0 16 16"><path d="M4.47.22A.749.749 0 0 1 5 0h6c.199 0 .389.079.53.22l4.25 4.25c.141.14.22.331.22.53v6a.749.749 0 0 1-.22.53l-4.25 4.25A.749.749 0 0 1 11 16H5a.749.749 0 0 1-.53-.22L.22 11.53A.749.749 0 0 1 0 11V5c0-.199.079-.389.22-.53Zm.84 1.28L1.5 5.31v5.38l3.81 3.81h5.38l3.81-3.81V5.31L10.69 1.5ZM8 4a.75.75 0 0 1 .75.75v3.5a.75.75 0 0 1-1.5 0v-3.5A.75.75 0 0 1 8 4Zm0 8a1 1 0 1 1 0-2 1 1 0 0 1 0 2Z"/></svg>',
-}
+} satisfies Record<(typeof DEFAULT_MARKERS)[number], string>
 
 export default (options: RemarkGitHubAlertsOptions = {}): Transformer<Root> => {
-    const {
-        markers = ["TIP", "NOTE", "IMPORTANT", "WARNING", "CAUTION"],
-        icons = DEFAULT_GITHUB_ICONS,
+    let {
+        markers,
+        icons,
         matchCaseSensitive = false,
-        titles = {},
+        titles,
         ignoreSquareBracket = false,
     } = options
 
-    const markerNameRE = markers === "*" ? "\\w+" : markers.join("|")
-    const RE = new RegExp(
+    if (!markers) markers = [...DEFAULT_MARKERS]
+    if (!icons) icons = DEFAULT_ICONS
+    if (!titles) titles = {}
+
+    const markerNamesRegex = markers === "*" ? "\\w+" : markers.join("|")
+    const markersRegex = new RegExp(
         ignoreSquareBracket
-            ? `^!(${markerNameRE})\\s?`
-            : `^\\[\\!(${markerNameRE})\\]\\s`,
+            ? `^\\!(${markerNamesRegex})([^\\n]*)?(\\n)?([\\s\\S]*)?`
+            : `^\\[\\!(${markerNamesRegex})\\]([^\\n]*)?(\\n)?([\\s\\S]*)?`,
         matchCaseSensitive ? "" : "i",
     )
 
     return (tree) => {
-        visit(tree, "blockquote", (node, index, parent) => {
-            const children = node.children as Paragraph[]
-            const firstParagraph = children[0]
-            if (!firstParagraph) return
-            let firstContent = firstParagraph.children[0]
-            if (!firstContent) return
-            if (
-                !("value" in firstContent) &&
-                "children" in firstContent &&
-                firstContent.children[0]
-            )
-                firstContent = firstContent.children[0]
+        visit(tree, "blockquote", (node) => {
+            if (!node.children.length) return
 
-            if (firstContent.type !== "text") return
-            const match = firstContent.value.match(RE)
+            const nodeFirstChildren = node.children[0]
+
+            if (nodeFirstChildren.type !== "paragraph") return
+            if (!nodeFirstChildren.children.length) return
+
+            const firstTextNode = nodeFirstChildren.children[0]
+
+            if (firstTextNode.type !== "text") return
+
+            const match = firstTextNode.value.match(markersRegex)
+
             if (!match) return
 
-            const type = match[1]?.toLowerCase() as keyof typeof icons
-            const title = match[2]?.trim() || (titles[type] ?? capitalize(type))
-            const icon = icons[type]
+            const matchedMarker =
+                markers === "*"
+                    ? match[1].toUpperCase()
+                    : (match[1] as keyof typeof icons)
+            const markerTitle =
+                match[2]?.trim() ||
+                titles[matchedMarker] ||
+                capitalize(matchedMarker)
+            const markerIcon = icons[matchedMarker]
 
-            if (index === undefined || !parent) return
+            firstTextNode.value = match[4] ?? ""
 
-            firstContent.value = firstContent.value
-                .slice(match[0].length)
-                .trimStart()
+            if (!firstTextNode.value) {
+                nodeFirstChildren.children.splice(0, 1)
+                if (!nodeFirstChildren.children.length) {
+                    node.children.splice(0, 1)
+                }
+            }
 
             node.data = {
                 hName: "div",
                 hProperties: {
-                    class: `remark-github-alerts remark-github-alerts-${type}`,
+                    class: `remark-github-alerts remark-github-alerts-${matchedMarker.toLowerCase()}`,
                 },
             }
+
             node.children = [
                 {
                     type: "paragraph",
@@ -122,18 +138,16 @@ export default (options: RemarkGitHubAlertsOptions = {}): Transformer<Root> => {
                     children: [
                         {
                             type: "html",
-                            value: icon,
+                            value: markerIcon,
                         },
                         {
                             type: "html",
-                            value: `<b>${title}</b>`,
+                            value: `<b>${markerTitle}</b>`,
                         },
                     ],
                 },
                 ...node.children,
             ]
         })
-
-        return tree
     }
 }
