c666bb0c36c079c08e0c7dff728f97831eccda44
babakfp <babak.bxf@gmail.com>
2024-08-06 23:02:15 +0330

    Fixed directives double label issue

--- src/transformers/unified/plugins/remark-directive.ts
+++ src/transformers/unified/plugins/remark-directive.ts
@@ -1,4 +1,4 @@
-import type { Root } from "mdast"
+import type { ParagraphData, Root } from "mdast"
 import type { ContainerDirective } from "mdast-util-directive"
 import type { Transformer } from "unified"
 import { visit } from "unist-util-visit"
@@ -36,41 +36,30 @@ export default (options?: Options): Transformer<Root> => {
                 hName: node.name === "details" ? "details" : "div",
                 hProperties: {
                     ...node.attributes,
-                    class: `remark-directive remark-directive-${node.name}${node.attributes?.class ?? ""}`,
+                    class: `remark-directive remark-directive-${node.name}${node.attributes?.class ? "" + node.attributes.class : ""}`,
                 },
             }
 
-            if (node.children.length) {
-                const firstChild = node.children[0]
-                if (firstChild.type === "paragraph") {
-                    if (firstChild.data?.directiveLabel) {
-                        setDirectiveLabel(true)
-                    } else {
-                        setDirectiveLabel()
-                    }
-                }
-            } else {
-                setDirectiveLabel()
+            if (
+                !(
+                    node.children[0].type === "paragraph" &&
+                    node.children[0].data?.directiveLabel
+                )
+            ) {
+                node.children.splice(0, 0, {
+                    type: "paragraph",
+                    data: { directiveLabel: true },
+                    children: [
+                        {
+                            type: "text",
+                            value: capitalize(node.name),
+                        },
+                    ],
+                })
             }
 
-            function setDirectiveLabel(isReplace?: boolean) {
-                if (node.name === "details") {
-                    node.children.splice(0, isReplace ? 1 : 0, {
-                        type: "html",
-                        value: `<summary>${capitalize(node.name)}</summary>`,
-                    })
-                } else {
-                    node.children.splice(0, 0, {
-                        type: "paragraph",
-                        data: { directiveLabel: true },
-                        children: [
-                            {
-                                type: "text",
-                                value: capitalize(node.name),
-                            },
-                        ],
-                    })
-                }
+            if (node.name === "details") {
+                ;(node.children[0].data as ParagraphData).hName = "summary"
             }
         })
     }
