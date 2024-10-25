184d6c0180fe98959665fbf9b9246331141923f0
babakfp <babak.bxf@gmail.com>
2024-10-25 19:27:21 +0330

    Added tests

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -1,22 +1,47 @@
 // sum.test.js
-import { expect, test } from "vitest"
+import { expect, it, test } from "vitest"
 import {
     moduleScriptRegex,
     normalScriptRegex,
 } from "../src/transformers/unified/plugins/remark-mdx-data-and-custom-elements.js"
 
-// Svelte <= 4
-test("script context module", () => {
+// script context module
+
+test("(svelte <= 4): script context module", () => {
     expect(moduleScriptRegex.test('<script context="module"></script>')).toBe(
         true,
     )
 })
 
-// Svelte >= 5
-test("script module", () => {
+it("(svelte <= 4): script context module", () => {
+    const match = '<script context="module"></script>'.match(moduleScriptRegex)
+    expect(match?.[1]).toBe('<script context="module">')
+    expect(match?.[3]).toBe("")
+    expect(match?.[4]).toBe("</script>")
+})
+
+// script module
+
+test("(svelte >= 5): script module", () => {
     expect(moduleScriptRegex.test("<script module></script>")).toBe(true)
 })
 
+it("(svelte >= 5): script module", () => {
+    const match = "<script module></script>".match(moduleScriptRegex)
+    expect(match?.[1]).toBe("<script module>")
+    expect(match?.[3]).toBe("")
+    expect(match?.[4]).toBe("</script>")
+})
+
+// script normal
+
 test("script normal", () => {
     expect(normalScriptRegex.test("<script></script>")).toBe(true)
+})
+
+it("script normal", () => {
+    const match = "<script></script>".match(normalScriptRegex)
+    expect(match?.[1]).toBe("<script>")
+    expect(match?.[3]).toBe("")
+    expect(match?.[4]).toBe("</script>")
 })
